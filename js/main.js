ca8a4e1e9a10fa9fd60b22248b8f8f57fd27ccc1
DevAcc <froststolen@gmail.com>
2025-11-05 11:26:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -434,6 +434,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       'img/misc/locked.png',
       'img/misc/locked_base.png',
       'img/misc/mysterious.png',
+	  ...Array.from({ length: 25 }, (_, i) => `img/mutations/m${i + 1}.png`),
     ],
     audio: [
       'sounds/coin_pickup.mp3',
