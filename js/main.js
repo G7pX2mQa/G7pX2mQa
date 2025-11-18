2681edec0b289fd696bf669c6036c8d6a8608efb
DevAcc <froststolen@gmail.com>
2025-11-18 12:31:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -382,6 +382,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     import('./game/coinPickup.js'),
     import('./ui/hudButtons.js'),
     import('./util/storage.js'),
+    import('./util/saveIntegrity.js'),
     import('./game/upgrades.js'),
     import('./util/audioCache.js'),
     import('./game/xpSystem.js'),
