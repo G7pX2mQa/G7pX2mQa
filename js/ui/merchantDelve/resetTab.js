7e0993f42981dc28fa5f7e9e025c99ca7c8d1cda
DevAcc <froststolen@gmail.com>
2025-11-20 09:52:56 -0500

    Update resetTab.js

--- js/ui/merchantDelve/resetTab.js
+++ js/ui/merchantDelve/resetTab.js
@@ -370,7 +370,7 @@ function buildPanel(panelEl) {
     <h3>Forge</h3>
     <p>
       Resets your Coins, Books, XP, Coin upgrades, and Book upgrades for Gold<br>
-	  Increase pending Gold amount by increasing Coins and XP Level
+	  Increase pending Gold amount by increasing Coins and XP Level<br>
       The button below shows how much Gold you will get upon reset
     </p>
   </div>
