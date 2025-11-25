ba0bb00a4ec58989a1d1b235a4ac3c101fce512c
DevAcc <froststolen@gmail.com>
2025-11-25 14:10:07 -0500

    Update upgrades.js

--- js/game/upgrades.js
+++ js/game/upgrades.js
@@ -2123,7 +2123,7 @@ const REGISTRY = [
     title: "Faster Coins",
     desc: "Increases coin spawn rate by +10% per level",
     lvlCap: 10,
-    baseCost: 1000,
+    baseCost: 10,
     costType: "coins",
     upgType: "NM",
     icon: "sc_upgrade_icons/faster_coins.png",
@@ -4114,6 +4114,7 @@ export function getHmNextMilestoneLevel(areaKey, upgId) {
 export function normalizeBigNum(value) {
   return bigNumFromLog10(approxLog10BigNum(value ?? 0));
 }
+
 
 
 
