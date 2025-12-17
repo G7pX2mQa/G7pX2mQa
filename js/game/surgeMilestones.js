563496b413fba5eb44749e0bfcdafcd4fd6385fb
DevAcc <froststolen@gmail.com>
2025-12-17 04:22:04 -0500

    Add files via upload

---
+++ js/game/surgeMilestones.js
@@ -0,0 +1,89 @@
+export const SURGE_MILESTONES = [
+  {
+    id: 1,
+    surgeLevel: 1,
+    description: [
+      "Placeholder text",
+      "Placeholder text"
+    ]
+  },
+  {
+    id: 2,
+    surgeLevel: 2,
+    description: [
+      "Placeholder text"
+    ]
+  },
+  {
+    id: 3,
+    surgeLevel: 5,
+    description: [
+      "Placeholder text",
+      "Another one"
+    ]
+  },
+  {
+    id: 4,
+    surgeLevel: 10,
+    description: [
+      "Future thing"
+    ]
+  },
+  {
+    id: 5,
+    surgeLevel: 20,
+    description: [
+      "Big future thing"
+    ]
+  },
+  {
+    id: 6,
+    surgeLevel: 50,
+    description: [
+      "Far future"
+    ]
+  },
+  {
+    id: 7,
+    surgeLevel: 100,
+    description: [
+      "Very far future"
+    ]
+  }
+];
+
+export function getVisibleMilestones(currentSurgeLevel) {
+  let currentLevel = 0;
+  
+  // Handle different number types safely
+  if (typeof currentSurgeLevel === 'number') {
+    currentLevel = currentSurgeLevel;
+  } else if (typeof currentSurgeLevel === 'bigint') {
+    // Safety clamp for very large BigInts to avoid Number conversion issues
+    // though surge levels likely won't exceed Number.MAX_SAFE_INTEGER
+    if (currentSurgeLevel > Number.MAX_SAFE_INTEGER) {
+      currentLevel = Number.MAX_SAFE_INTEGER;
+    } else {
+      currentLevel = Number(currentSurgeLevel);
+    }
+  } else if (currentSurgeLevel && typeof currentSurgeLevel.toString === 'function') {
+      try {
+          const val = Number(currentSurgeLevel.toString());
+          if (!isNaN(val)) currentLevel = val;
+      } catch {}
+  }
+  
+  const reached = [];
+  const future = [];
+  
+  for (const m of SURGE_MILESTONES) {
+    if (m.surgeLevel <= currentLevel) {
+      reached.push(m);
+    } else {
+      future.push(m);
+    }
+  }
+  
+  // Return all reached + up to 2 future
+  return [...reached, ...future.slice(0, 2)];
+}
