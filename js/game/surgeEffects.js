83b274c772ffcf96a9f57c6a6fe51a243b7766b9
DevAcc <froststolen@gmail.com>
2026-01-28 10:12:35 -0500

    Update surgeEffects.js

--- js/game/surgeEffects.js
+++ js/game/surgeEffects.js
@@ -133,8 +133,8 @@ export function getSurge6WealthMultipliers() {
          } catch {}
       }
       
-      // Formula: 2 ^ (log10(amount) / 25)
-      const power = finalLog10 / 25;
+      // Formula: 2 ^ (log10(amount) / 10)
+      const power = finalLog10 / 10;
       if (power <= 0) return BigNum.fromInt(1);
       
       // 2^power = 10^(power * log10(2))
