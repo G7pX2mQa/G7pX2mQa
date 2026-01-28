25b2e9cce60cd1897dbcac4451cf0d7a475ca141
DevAcc <froststolen@gmail.com>
2026-01-28 10:07:33 -0500

    Update surgeMilestones.js

--- js/game/surgeMilestones.js
+++ js/game/surgeMilestones.js
@@ -30,7 +30,7 @@ export const SURGE_MILESTONES = [
     id: 4,
     surgeLevel: 4,
     description: [
-      `Multiplies MP value by <span style="color:#00e5ff">${formatNumber(BigNum.fromInt(1e14))}x</span>`
+      `Multiplies MP value by <span style="color:#00e5ff">${formatNumber(BigNum.fromInt(4.444e12))}x</span>`
     ]
   },
     {
