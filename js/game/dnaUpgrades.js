d81c5bf854fcdfee71ca90ba1cbd2b8982a1e918
DevAcc <froststolen@gmail.com>
2026-02-08 19:53:02 -0500

    Update dnaUpgrades.js

--- js/game/dnaUpgrades.js
+++ js/game/dnaUpgrades.js
@@ -1,2 +1,36 @@
 export const DNA_AREA_KEY = 'dna';
-export const REGISTRY = [];
+
+export const REGISTRY = [
+  {
+    area: DNA_AREA_KEY,
+    id: 1,
+    title: "DNA Coin Value",
+    desc: "Multiplies Coin value by 1.1x per level",
+    lvlCap: 1000,
+    baseCost: 1000,
+    costType: "dna",
+    upgType: "HM",
+    scalingPreset: 'HM',
+    icon: "sc_upg_icons/coin_val_dna.webp",
+    baseIconOverride: "img/currencies/dna/dna_base.webp",
+    effectType: "coin_value",
+    _dnaEffectVal: 1.1,
+    _costScaling: 'HM'
+  },
+  {
+    area: DNA_AREA_KEY,
+    id: 2,
+    title: "DNA XP Value",
+    desc: "Multiplies XP value by 1.1x per level",
+    lvlCap: 1000,
+    baseCost: 1000,
+    costType: "dna",
+    upgType: "HM",
+    scalingPreset: 'HM',
+    icon: "sc_upg_icons/xp_val_dna.webp",
+    baseIconOverride: "img/currencies/dna/dna_base.webp",
+    effectType: "xp_value",
+    _dnaEffectVal: 1.1,
+    _costScaling: 'HM'
+  }
+];
