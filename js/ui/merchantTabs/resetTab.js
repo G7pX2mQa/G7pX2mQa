cf0d4a4be8e6d068bd657a270a8397b51467edbd
DevAcc <froststolen@gmail.com>
2026-02-17 10:56:17 -0500

    Update resetTab.js

--- js/ui/merchantTabs/resetTab.js
+++ js/ui/merchantTabs/resetTab.js
@@ -412,13 +412,13 @@ export function computePendingDnaFromInputs(labLevelBn, xpLevelBn, isSurge9Overr
     }
 
     if (isSurgeActive(14)) {
-        // Multiplier: 14.14e12
+        // Multiplier: 14.14e6
         if (isSurgeActive(8)) {
             const effectiveNerf = getEffectiveTsunamiNerf();
-            const log14 = Math.log10(14.14e12);
+            const log14 = Math.log10(14.14e6);
             logMultiplier += log14 * effectiveNerf;
         } else {
-            logMultiplier += Math.log10(14.14e12);
+            logMultiplier += Math.log10(14.14e6);
         }
     }
 
@@ -933,13 +933,13 @@ function recomputePendingDna() {
     }
 
     if (isSurgeActive(14)) {
-        // Multiplier: 14.14e12
+        // Multiplier: 14.14e6
         if (isSurgeActive(8)) {
             const effectiveNerf = getEffectiveTsunamiNerf();
-            const log14 = Math.log10(14.14e12);
+            const log14 = Math.log10(14.14e6);
             logMultiplier += log14 * effectiveNerf;
         } else {
-            logMultiplier += Math.log10(14.14e12);
+            logMultiplier += Math.log10(14.14e6);
         }
     }
 
