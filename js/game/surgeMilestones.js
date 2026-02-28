64e8e11de3b9fefd604f61986ce73518e4a35c0c
DevAcc <froststolen@gmail.com>
2026-02-28 12:22:04 -0500

    Update surgeMilestones.js

--- js/game/surgeMilestones.js
+++ js/game/surgeMilestones.js
@@ -14,7 +14,7 @@ export const getSurge10Description = (slot) => {
 import { formatNumber, formatMultForUi } from '../util/numFormat.js';
 import { BigNum } from '../util/bigNum.js';
 import { bigNumFromLog10, approxLog10BigNum } from '../util/bigNum.js';
-import { getTsunamiNerf, getEffectiveTsunamiNerf, getSurge15Multiplier, getSurge15Divisor, getSurge21Multiplier, getSurge21BonusPercentage, getSurge23Multiplier, getSurge23BonusPercentage, getSurge25Multiplier, getSurge25BonusPercentage, getBookProductionRate, getSurge6WealthMultipliers} from "./surgeEffects.js";
+import { getTsunamiNerf, getEffectiveTsunamiNerf, getSurge15Multiplier, getSurge15Divisor, getSurge21Multiplier, getSurge21BonusPercentage, getSurge23Multiplier, getSurge23BonusPercentage, getSurge25Multiplier, getSurge25BonusPercentage, getSurge27Multiplier, getSurge27BonusPercentage, getBookProductionRate, getSurge6WealthMultipliers} from "./surgeEffects.js";
 import { getTsunamiResearchBonus, getResearchNodeLevel } from './labNodes.js';
 import { getActiveSlot } from '../util/storage.js';
 
@@ -215,6 +215,14 @@ export const SURGE_MILESTONES = [
     affectedByTsunami: true,
     description: [
       "Increases Gold value by <span style=\"color:#00e5ff\">+100%</span> per Surge milestone after 24"
+    ]
+  },
+  {
+    id: 24,
+    surgeLevel: 27,
+    affectedByTsunami: true,
+    description: [
+      "Increases Magic value by <span style=\"color:#00e5ff\">+100%</span> per Surge milestone after 26"
     ]
   }
 ];
@@ -615,6 +623,29 @@ export function getVisibleMilestones(currentSurgeLevel, pendingVals = {}) {
             formattedBonus = "Infinity";
         } else {
             const diffBN = currentLevelBN.sub(BigNum.fromInt(20));
+            const totalBonusPct = diffBN.cmp(0) > 0 ? diffBN.mulDecimal(baseBonusPct) : BigNum.fromInt(0);
+            formattedBonus = formatNumber(totalBonusPct);
+        }
+        milestone.description.push(`Current bonus: +${formattedBonus}%`);
+      }
+    }
+
+    if (m.id === 24) {
+      if (milestone === m) {
+          milestone = { ...m, description: [...m.description] };
+      }
+
+      const baseBonusPct = getSurge27BonusPercentage();
+      const formattedBaseBonus = formatNumber(BigNum.fromAny(baseBonusPct));
+      milestone.description[0] = `Increases Magic value by <span style="color:#00e5ff">+${formattedBaseBonus}%</span> per Surge milestone after 26`;
+
+      if (currentLevel >= 27) {
+        const mult = getSurge27Multiplier();
+        let formattedBonus = "0";
+        if (mult.isInfinite?.() || currentLevelBN.isInfinite?.()) {
+            formattedBonus = "Infinity";
+        } else {
+            const diffBN = currentLevelBN.sub(BigNum.fromInt(26));
             const totalBonusPct = diffBN.cmp(0) > 0 ? diffBN.mulDecimal(baseBonusPct) : BigNum.fromInt(0);
             formattedBonus = formatNumber(totalBonusPct);
         }
