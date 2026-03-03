95f86662c10de01724fbaba6f4b744e6ea5a8a6b
DevAcc <froststolen@gmail.com>
2026-03-03 13:59:21 -0500

    Update coinPickup.js

--- js/game/coinPickup.js
+++ js/game/coinPickup.js
@@ -914,7 +914,7 @@ export function initCoinPickup({
     const xpEnabled = typeof isXpSystemUnlocked === 'function' ? isXpSystemUnlocked() : true;
     const mutEnabled = typeof isMutationUnlocked === 'function' && isMutationUnlocked();
 
-    const MAX_VISUALS = 10;
+    const MAX_VISUALS = 15;
     let visualCount = 0;
 
     for (const item of items) {
