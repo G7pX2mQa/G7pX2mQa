124dcd45ece79bb9d43bd59f77b09be46c4382f9
DevAcc <froststolen@gmail.com>
2026-03-03 14:12:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -640,6 +640,7 @@ images: [
   'img/sc_upg_icons/coin_val_hm1.webp',
   'img/sc_upg_icons/coin_val_hm2.webp',
   'img/sc_upg_icons/coin_val_hm3.webp',
+  'img/sc_upg_icons/dna_val_dna.webp',
   'img/sc_upg_icons/faster_coins1.webp',
   'img/sc_upg_icons/faster_coins2.webp',
   'img/sc_upg_icons/faster_coins3.webp',
