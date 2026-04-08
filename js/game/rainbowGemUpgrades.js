3319cbc3d39b1fcae2b666c499d6c25ee54f7e53
DevAcc <froststolen@gmail.com>
2026-04-07 21:02:58 -0400

    Add files via upload

---
+++ js/game/rainbowGemUpgrades.js
@@ -0,0 +1,37 @@
+export const RAINBOW_GEM_AREA_KEY = 'rainbow_gem_shop';
+
+export const RAINBOW_GEM_UPGRADES = [
+  {
+    area: RAINBOW_GEM_AREA_KEY,
+    id: 1,
+    title: "Placeholder upgrade for fonts",
+    desc: "A placeholder upgrade.",
+    lvlCap: 1,
+    costType: "rainbowGems",
+    upgType: "NM",
+    costAtLevel: () => 100,
+    iconPath: "img/currencies/coin/coin.webp",
+  },
+  {
+    area: RAINBOW_GEM_AREA_KEY,
+    id: 2,
+    title: "Placeholder upgrade for trails",
+    desc: "A placeholder upgrade.",
+    lvlCap: 1,
+    costType: "rainbowGems",
+    upgType: "NM",
+    costAtLevel: () => 100,
+    iconPath: "img/currencies/coin/coin.webp",
+  },
+  {
+    area: RAINBOW_GEM_AREA_KEY,
+    id: 3,
+    title: "Placeholder upgrade for magnets",
+    desc: "A placeholder upgrade.",
+    lvlCap: 1,
+    costType: "rainbowGems",
+    upgType: "NM",
+    costAtLevel: () => 100,
+    iconPath: "img/currencies/coin/coin.webp",
+  }
+];
