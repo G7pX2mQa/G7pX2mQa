26ca4958d74b3462dcce29ed5daa7949f2ad8907
DevAcc <froststolen@gmail.com>
2026-04-09 23:31:09 -0400

    Update rainbowGemUpgrades.js

--- js/game/rainbowGemUpgrades.js
+++ js/game/rainbowGemUpgrades.js
@@ -7,50 +7,50 @@ export const RAINBOW_GEM_UPGRADES = [
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 1,
-    title: "Classic Font",
-    desc: "Changes the game's font to Times New Roman.",
+    title: "Times New Roman",
+    desc: "Changes the game's font to Times New Roman",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "font",
     costAtLevel: () => 100,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
   },
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 2,
     title: "Bronze Trail",
-    desc: "Changes the cursor trail color to bronze.",
+    desc: "Changes the cursor trail to a bronze color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "trail",
     costAtLevel: () => 100,
-    icon: "img/currencies/coin/coin.webp"
+    icon: "img/currencies/rainbow_gem.webp"
   },
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 3,
     title: "Bronze Magnet",
-    desc: "Changes the magnet radius indicator color to bronze.",
+    desc: "Changes the magnet indicator to a bronze color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "magnet",
     costAtLevel: () => 100,
-    icon: "img/currencies/coin/coin.webp"
+    icon: "img/currencies/rainbow_gem.webp"
   },
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 4,
     title: "Arial Font",
-    desc: "Changes the game's font to Arial.",
+    desc: "Changes the game's font to Arial",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "font",
     costAtLevel: () => 200,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 2) {
@@ -73,13 +73,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 5,
     title: "Silver Trail",
-    desc: "Changes the cursor trail color to silver.",
+    desc: "Changes the cursor trail to a silver color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "trail",
     costAtLevel: () => 200,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 2) {
@@ -102,13 +102,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 6,
     title: "Silver Magnet",
-    desc: "Changes the magnet radius indicator color to silver.",
+    desc: "Changes the magnet indicator to a silver color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "magnet",
     costAtLevel: () => 200,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 2) {
@@ -130,14 +130,14 @@ export const RAINBOW_GEM_UPGRADES = [
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 7,
-    title: "Courier New Font",
-    desc: "Changes the game's font to Courier New.",
+    title: "Courier New",
+    desc: "Changes the game's font to Courier New",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "font",
     costAtLevel: () => 300,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 3) {
@@ -160,13 +160,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 8,
     title: "Gold Trail",
-    desc: "Changes the cursor trail color to gold.",
+    desc: "Changes the cursor trail to a gold color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "trail",
     costAtLevel: () => 300,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 3) {
@@ -189,13 +189,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 9,
     title: "Gold Magnet",
-    desc: "Changes the magnet radius indicator color to gold.",
+    desc: "Changes the magnet indicator to a gold color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "magnet",
     costAtLevel: () => 300,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 3) {
@@ -217,14 +217,14 @@ export const RAINBOW_GEM_UPGRADES = [
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 10,
-    title: "Verdana Font",
-    desc: "Changes the game's font to Verdana.",
+    title: "Verdana",
+    desc: "Changes the game's font to Verdana",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "font",
     costAtLevel: () => 400,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 4) {
@@ -247,13 +247,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 11,
     title: "Sapphire Trail",
-    desc: "Changes the cursor trail color to sapphire.",
+    desc: "Changes the cursor trail to a sapphire color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "trail",
     costAtLevel: () => 400,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 4) {
@@ -276,13 +276,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 12,
     title: "Sapphire Magnet",
-    desc: "Changes the magnet radius indicator color to sapphire.",
+    desc: "Changes the magnet indicator to a sapphire color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "magnet",
     costAtLevel: () => 400,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 4) {
@@ -304,14 +304,14 @@ export const RAINBOW_GEM_UPGRADES = [
   {
     area: RAINBOW_GEM_AREA_KEY,
     id: 13,
-    title: "Tahoma Font",
-    desc: "Changes the game's font to Tahoma.",
+    title: "Tahoma",
+    desc: "Changes the game's font to Tahoma",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "font",
     costAtLevel: () => 500,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 5) {
@@ -334,13 +334,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 14,
     title: "Emerald Trail",
-    desc: "Changes the cursor trail color to emerald.",
+    desc: "Changes the cursor trail to an emerald color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "trail",
     costAtLevel: () => 500,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 5) {
@@ -363,13 +363,13 @@ export const RAINBOW_GEM_UPGRADES = [
     area: RAINBOW_GEM_AREA_KEY,
     id: 15,
     title: "Emerald Magnet",
-    desc: "Changes the magnet radius indicator color to emerald.",
+    desc: "Changes the magnet indicator to an emerald color",
     lvlCap: 1,
     costType: "rainbowGems",
     upgType: "TM",
     modType: "magnet",
     costAtLevel: () => 500,
-    icon: "img/currencies/coin/coin.webp",
+    icon: "img/currencies/rainbow_gem.webp",
     computeLockState(ctx) {
       const mState = getMutationState();
       if (!mState.unlocked || levelBigNumToNumber(mState.level) < 5) {
