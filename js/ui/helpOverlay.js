98fb25ee95e6c31fcfe1a217dc2375b92fbcef9a
DevAcc <froststolen@gmail.com>
2026-04-16 21:27:04 -0400

    Update helpOverlay.js

--- js/ui/helpOverlay.js
+++ js/ui/helpOverlay.js
@@ -68,9 +68,9 @@ const HELP_ENTRIES = [
     id: 1,
     title: "Intro",
     icon: "img/currencies/coin/coin.webp",
-    tldr: "Placeholder TLDR for Intro.",
-    progressionGoal: "Placeholder Progression goal for Intro.",
-    text: "Placeholder text for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for Placeholder TLDR for .",
+    tldr: "Collect Coins; Buy upgrades; Make numbers go up",
+    progressionGoal: "Reach XP Level 31 and unlock a certain upgrade",
+    text: "You probably don't need a help text for this section of the game (Intro), because it's very straightforward and designed to be intuitive. But if you haven't figured it out already, the core gameplay mechanic of this game is collecting Coins, which you can do by hovering over the Coins with your cursor. Your native cursor is hidden while on the playfield (the area the Coins settle into), and instead a particle trail is constantly drawn where your cursor is on the playfield. Moving past that, the main way you will progress through the game is through buying Shop upgrades and interacting with the Merchant when necessary. You'll need to left-click on an upgrade's icon in the Shop to access the upgrade's overlay, from which you can spend currency on that upgrade to make numbers go up faster. There are also a few shortcuts you can perform on upgrades like right-click to buy max and more, but you can find more information in the Shortcuts section of the Stats & Settings menu. Anyway, keep collecting those Coins!",
     themeClass: "is-intro",
     isVisible: () => true // Always unlocked
   },
@@ -78,9 +78,9 @@ const HELP_ENTRIES = [
     id: 2,
     title: "Forge",
     icon: "img/misc/forge.webp",
-    tldr: "Placeholder TLDR for Forge.",
-    progressionGoal: "Placeholder Progression goal for Forge.",
-    text: "Placeholder text for Forge.",
+    tldr: "Reset immediately; Spend Gold wisely; Reset more when progress is slow",
+    progressionGoal: "Reach XP Level 101 and unlock a certain upgrade",
+    text: "Now the game's starting to open up. Forge is this game's first reset layer of many. If you're unfamiliar with the concept, a reset layer is when you reset all of your progress before this point but exchange it for a reward. You'll never have to worry about waiting extra time to perform your first reset. You will quickly recover from it and obtain game-changing new multipliers as a result (pro tip: after your first Forge reset, speak with the Merchant for free Gold!). The Forge reset gives Gold when performed, based on your Coins and XP Level at the time of performing it. The first Forge reset unlocks new upgrades where this Gold currency can be spent, it also unlocks a new Mutation system where you can mutate Coins and double the value of Coins and XP each time your Mutation increases. The core loop of this section of the game (Forge) is to reach a high XP Level, then when progress is slow, perform a Forge reset for tons of Gold. The first Forge reset also unlocked a new type of Shop upgrade: the Milestone-type upgrade. The upgrade does a good job of explaining what it does, you can find more information in the upgrade overlay for the upgrade Endless XP. Have fun discovering all of the unique mutated Coin sprites!",
     themeClass: "is-forge",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(2)) return true;
@@ -98,9 +98,9 @@ const HELP_ENTRIES = [
     id: 3,
     title: "Infuse",
     icon: "img/misc/infuse.webp",
-    tldr: "Placeholder TLDR for Infuse.",
-    progressionGoal: "Placeholder Progression goal for Infuse.",
-    text: "Placeholder text for Infuse.",
+    tldr: "Reset immediately; Spend Magic wisely; Reset more when progress is slow",
+    progressionGoal: "Reach XP Level 201 and unlock a certain upgrade",
+    text: "Similar to Forge, except now you get Magic on reset instead of Gold. Speak with the Merchant after your first Infuse reset to get some extra Magic for free, and also check out the Workshop tab to get some very generous automation, but there's nothing much else to say; just perform your first Infuse reset right away. There is one slight difference, which is that Infuse scales based on Coins and MP (collective MP gained throughout all Mutations), so just know that to get more Magic, you'll want to maximize your MP value as much as possible. You should know how this works though, just perform more Forge resets for Gold and perform Infuse resets when you've done a satisfactory amount of Forge resets.",
     themeClass: "is-infuse",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(3)) return true;
@@ -118,9 +118,9 @@ const HELP_ENTRIES = [
     id: 4,
     title: "Surge",
     icon: "img/misc/surge.webp",
-    tldr: "Placeholder TLDR for Surge.",
-    progressionGoal: "Placeholder Progression goal for Surge.",
-    text: "Placeholder text for Surge.",
+    tldr: "Perform even more resets; Waves do not function like a normal currency; Reach new milestones when possible",
+    progressionGoal: "Reach Surge Milestone 8",
+    text: "Congrats on reaching this point! Past this point is when the game starts to get real. Surge is a milestone-based reset layer, meaning instead of resetting for a currency to spend that currency on upgrades, you instead reset to unlock powerful milestones. The descriptive text on the Surge reset card does a good job of explaining how Surge works, but what should you be trying to do to progress further in the game now? Well, the only way to progress now is to keep unlocking new Surge Milestones. To do this, you'll need to keep performing Surge resets when you have enough pending Waves to reach the next milestone. Waves earned from the Surge reset are based on a lot of things, but the most important thing to focus on is XP Level, because that is the most significant factor in earning many Waves. You can unlock multiple milestones from one Surge reset due to how it works, but you don't really need to be doing that in these early Surge Milestones; just reach the next milestone when possible. The first Surge reset also unlocks the Warp tab, which is useful if you ever want to skip any sort of timewall, but there aren't any real timewalls in this game anyway, so you can use Warps at your convenience. So keep unlocking new Surge Milestones and your Coins will reach numbers they never have before.",
     themeClass: "is-surge",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(4)) return true;
@@ -138,9 +138,9 @@ const HELP_ENTRIES = [
     id: 5,
     title: "Lab",
     icon: "img/stats/rp/rp.webp",
-    tldr: "Placeholder TLDR for Lab.",
-    progressionGoal: "Placeholder Progression goal for Lab.",
-    text: "Placeholder text for Lab.",
+    tldr: "Research Lab Nodes; Get more Coins to research Lab Nodes faster; Work to restore the Tsunami Exponent which nerfs all your Surge Milestones",
+    progressionGoal: "Research Lab Node 4",
+    text: "That whole Tsunami sequence was pretty cool huh? Anyway, you must have a lot of questions about the Lab tab and everything inside it if you're reading this now. The Merchant explained briefly what to do but wouldn't elaborate further when you say things like, \"I don't understand.\", or \"Tsunami sacrifice?\", or \"???\", and this help text will elaborate on all three of those confusions. First, let's start by explaining what is the Lab? First of all, if you don't know the controls, look in the bottom left corner to see what you need to do to interact with the large grid containing the Lab Nodes. Focusing on gameplay, you will need to research various Lab Nodes which will provide huge multipliers to all sorts of things. Coin value, XP value, Gold value, Magic value, you name it, there's probably a Lab Node for it. Researching these Lab Nodes takes time, and the time it takes for any given level of a Lab Node to finish is dependent on how fast you can reach the required RP (Research Progress) for that level. Each Lab Node needs to be actively being researched or not, toggled ON or OFF, and only one node can be researched at a time; sometimes you will be required to research multiple nodes alongside each other to progress quickly. The main way to increase how quickly you can research Lab Nodes is by increasing your Lab Level, which scales based on your highest reached amount of Coins. Double research speed for each Lab Level, but the cost to increase this Lab Level increases tenfold each time. The Merchant also vaguely mentioned that your Surge Milestones were temporarily sacrificed to the Tsunami, but this isn't very clear. And you might've seen a thing called Tsunami Exponent near the top of the Lab tab also, so what's that? Basically, the Surge Milestones are so powerful that they had to be stopped. Invoking the Tsunami has applied a ^0.00 exponent to all Surge Milestones that involve multipliers of any kind, and you can view this effect on the Surge Milestones right now if you want to. This means that a milestone that used to boost your Coins, XP, and MP value by 10x now boosts those things by 10^0.00=1.00x, resulting in no boost from the milestone. The good news is you can restore this Tsunami Exponent from 0.00 all the way back to 1.00 through researching certain Lab Nodes, but it will be a long time until you reach that point. Immediately following the unlock of the eighth Surge Milestone, you'll be unable to gather any new Surge Milestones right away, but you'll need to perform the other resets in order to get as many Coins as possible and increase your Lab Level as high as possible. You'll unlock something new after researching Lab Node 4.",
     themeClass: "is-lab",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(5)) return true;
@@ -155,9 +155,9 @@ const HELP_ENTRIES = [
     id: 6,
     title: "Experiment",
     icon: "img/misc/experiment.webp",
-    tldr: "Placeholder TLDR for Experiment.",
-    progressionGoal: "Placeholder Progression goal for Experiment.",
-    text: "Placeholder text for Experiment.",
+    tldr: "Combine all four currently unlocked resets to progress further; keep unlocking more Surge Milestones",
+    progressionGoal: "Reach Surge Milestone 20",
+    text: "Now that you have a solid understanding of how the Lab works, it's time to introduce another reset layer! Well, sort of. It would be really evil if this new reset were to reset Surge Milestones, but it doesn't. Instead, it only resets everything Surge does and also the entire Lab. Okay okay, that sounds bad, and maybe it is somewhat bad, but you'll get more multipliers from doing it, so you can't complain. The currency gained from this reset is called DNA and it can be used to buy some powerful Milestone-type upgrades that boost Coins, XP, and other stuff locked behind certain Surge Milestones. The reset itself resets Lab Nodes and Lab Level, so you're starting from square one again, but this time you'll have stronger multipliers and also access to Lab Nodes past the fourth one. In order to keep progressing further, you'll need to combine Forge resets with Infuse resets with Surge resets with Experiment resets with researching Lab Nodes and collecting Coins and all of this stuff, it's so exciting. You'll keep repeating this loop for a while and unlock new fun Surge Milestones along the way until you unlock the next major feature.",
     themeClass: "is-experiment",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(6)) return true;
@@ -172,9 +172,9 @@ const HELP_ENTRIES = [
     id: 7,
     title: "Flow",
     icon: "img/stats/fp/fp.webp",
-    tldr: "Placeholder TLDR for Flow.",
-    progressionGoal: "Placeholder Progression goal for Flow.",
-    text: "Placeholder text for Flow.",
+    tldr: "NGU Idle but better; Toggle Flow States of Waterwheels to passively gain multipliers for things; Levels of Waterwheels don't scale in requirement",
+    progressionGoal: "Reach Surge Milestone 120",
+    text: "Are you familiar with NGU Idle? The whole Flow tab is basically the early game of NGU Idle but nothing is capped and the numbers get a lot crazier. If you couldn't decipher the poem at the top of the Flow tab that was supposed to explain what it is about, don't worry, it's very vague in some parts. Basically, you must activate the Flow State of various Waterwheels and this will earn you more multipliers for the thing that the Waterwheel boosts. Pretend that each of these Waterwheels initially sits idle in a channel of water and needs you to make the water in the channels flow. But to add some strategy, you can only have one Waterwheel actively leveling up (Flow State toggled ON) at a time. As you unlock more Waterwheels, you will have to make decisions between which Waterwheels you want to flow and when. Each level of a Waterwheel boosts the value of its respective currency or stat by +100%, so level 5 of a Waterwheel would boost the thing by +500%. Waterwheels that have their Flow State toggled ON passively gain a certain amount of FP (Flow Progress) each second, and every Waterwheel has a different amount of FP required to level them up. But, the FP required to level up any Waterwheel is constant (it never scales as the level grows), leading to the potential for very high levels on the Waterwheels. You can unlock new Waterwheels by completing whatever requirement is listed as the unlock requirement on each Waterwheel; some may just require a certain amount of levels of the previous Waterwheel while some may require something else. This feature was intentionally designed to not be too active-focused, as you still have Lab Nodes and Surge Milestones and everything else to worry about, but you will need to be mindful of the fact that you will need to juggle the various Waterwheels as you unlock more of them. The poem isn't lying when it says the Waterwheels will wake the strongest multipliers that The Cove has ever seen.",
     themeClass: "is-flow",
     isVisible: () => {
         if (isHelpEntryPermanentlyUnlocked(7)) return true;
