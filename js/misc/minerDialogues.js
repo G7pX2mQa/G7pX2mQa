db860cd5800d2ecc6adafb0109af2f11456418c9
DevAcc <froststolen@gmail.com>
2026-05-03 13:57:47 -0400

    Add files via upload

---
+++ js/misc/minerDialogues.js
@@ -0,0 +1,8 @@
+export const MINER_DIALOGUES = [
+    {
+        id: "miner_initial",
+        nodes: [
+            { id: "start", text: "Miner dialogue placeholder", options: [] }
+        ]
+    }
+];
