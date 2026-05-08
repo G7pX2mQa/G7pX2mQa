f5794d2c769671afc7952a767ac5705c3e7da5f1
DevAcc <froststolen@gmail.com>
2026-05-08 17:28:37 -0400

    Update mapOverlay.js

--- js/ui/mapOverlay.js
+++ js/ui/mapOverlay.js
@@ -24,7 +24,7 @@ export function setNodeLocked(id, locked) {
 export function getMapNodes() {
     return [
         { id: 'cove', areaId: AREAS.STARTER_COVE, name: 'The Cove', icon: 'img/currencies/coin/coin_plus_base.webp', top: '21%', left: '50%', defaultLocked: false },
-        { id: 'cavern', areaId: AREAS.UNDERWATER_CAVERN, name: 'Underwater Cavern', icon: 'img/misc/scrap_plus_base.webp', top: '36%', left: '75%', defaultLocked: true, previousNodeId: 'cove' },
+        { id: 'cavern', areaId: AREAS.UNDERWATER_CAVERN, name: 'Underwater Cavern', icon: 'img/currencies/scrap/scrap_plus_base.webp', top: '36%', left: '75%', defaultLocked: true, previousNodeId: 'cove' },
         { id: 'coral', areaId: null, name: 'Coral Reef', icon: 'img/misc/mysterious_plus_base.webp', top: '51%', left: '25%', defaultLocked: true },
         { id: 'depths', areaId: null, name: 'Deep Depths', icon: 'img/misc/mysterious_plus_base.webp', top: '85%', left: '50%', defaultLocked: true }
     ];
