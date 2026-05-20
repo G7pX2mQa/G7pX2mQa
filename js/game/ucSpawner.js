94f69de6bcd887d919240d4cefc0e8ea0cad9925
DevAcc <froststolen@gmail.com>
2026-05-19 22:29:27 -0400

    Update ucSpawner.js

--- js/game/ucSpawner.js
+++ js/game/ucSpawner.js
@@ -1,3 +1,5 @@
+import { BigNum } from '../util/bigNum.js';
+import { getDpState } from './dpSystem.js';
 import { createBaseSpawner, CUBIC_BEZIER, getImage } from './spawnerCore.js';
 import { IS_MOBILE } from '../main.js';
 import { playAudio } from '../util/audioManager.js';
