c08edaa1f93e132b2a773eeba581d4c001a465e9
DevAcc <froststolen@gmail.com>
2026-05-22 09:30:46 -0400

    Update minerDialogues.js

--- js/misc/minerDialogues.js
+++ js/misc/minerDialogues.js
@@ -2,7 +2,7 @@ export const MINER_DIALOGUES = {
   0: {
     start: 'n0',
     nodes: {
-      n0: { type: 'line', say: 'Who… Are… You...?', next: 'c0' },
+      n0: { type: 'line', say: 'Who... Are... You...?', next: 'c0' },
 
       c0: {
         type: 'choice',
@@ -15,17 +15,17 @@ export const MINER_DIALOGUES = {
 
       m1a: {
         type: 'line',
-        say: 'It has been so long… I never thought I would see another <span style="color:#00e5ff">Player</span> again…',
+        say: 'It has been so long... I never thought I would see another <span style="color:#00e5ff">Player</span> again...',
         next: 'c1a'
       },
       m1b: {
         type: 'line',
-        say: 'Miner… That is what they have called me…',
+        say: 'Miner... That is what they have called me...',
         next: 'c1b'
       },
       m1c: {
         type: 'line',
-        say: 'I never thought I’d see the day…',
+        say: 'I never thought I’d see the day...',
         next: 'c1c'
       },
 
