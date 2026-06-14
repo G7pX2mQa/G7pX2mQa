b86d86a33d674a89690a79baab4fa077756934ed
DevAcc <froststolen@gmail.com>
2026-06-14 03:51:37 -0400

    Update offlinePanel.js

--- js/game/offlinePanel.js
+++ js/game/offlinePanel.js
@@ -207,11 +207,11 @@ export const RESOURCE_REGISTRY = [
             }
             
             return {
-                level,
+                level: BigNum.fromAny(level),
                 progress,
                 requirement: req,
                 isUnlocked,
-                ratio
+                ratio: Math.min(1, Math.max(0, ratio))
             };
         }
     },
@@ -260,7 +260,7 @@ export const RESOURCE_REGISTRY = [
             };
         }
     },
-    { key: 'dp_levels', icon: 'img/stats/dp/dp.webp',            singular: 'Meter of Depth', plural: 'Meters of Depth', type: 'levelStat', color: '#A98060', barText: 'Depth:<span class="dp-level-value">{val}</span>m' },
+    { key: 'dp_levels', icon: 'img/stats/dp/dp.webp',            singular: 'Meter of Depth', plural: 'Meters of Depth', type: 'levelStat', color: '#A98060', barText: 'Depth: <span class="dp-level-value">{val}</span>m', noTextGap: true },
     { key: 'cores', bgGradient: 'linear-gradient(to bottom, #0a0a0a 0%, #1a1a1a 15%, #2a2a2a 50%, #1a1a1a 85%, #0a0a0a 100%)',      icon: 'img/currencies/core/core.webp', baseIcon: 'img/currencies/core/core_plus_base.webp',   singular: 'Core',     plural: 'Cores', type: 'currency' },
     { key: 'crystals', bgGradient: 'linear-gradient(to bottom, #943276 0%, #bd53a3 15%, #e979d0 50%, #bd53a3 85%, #943276 100%)',      icon: 'img/currencies/crystal/crystal.webp', baseIcon: 'img/currencies/crystal/crystal_plus_base.webp',   singular: 'Crystal',     plural: 'Crystals', type: 'currency' },
 ];
