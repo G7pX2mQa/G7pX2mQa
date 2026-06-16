01bd784d26ba6e0ebae5adeb1df7ce0c437c2770
DevAcc <froststolen@gmail.com>
2026-06-15 22:48:51 -0400

    Update offlinePanel.js

--- js/game/offlinePanel.js
+++ js/game/offlinePanel.js
@@ -260,7 +260,7 @@ export const RESOURCE_REGISTRY = [
             };
         }
     },
-    { key: 'dp_levels', icon: 'img/stats/dp/dp.webp',            singular: 'meter of Depth', plural: 'meters of Depth', type: 'levelStat', color: '#A98060', barText: 'Depth: <span class="dp-level-value">{val}</span>m', noTextGap: true },
+    { key: 'dp_levels', icon: 'img/stats/dp/dp.webp',            singular: 'meter of Depth', plural: 'meters of Depth', type: 'levelStat', color: '#A98060', barText: 'Depth:<span class="dp-level-value">{val}</span>m', noTextGap: true },
     { 
         key: 'pp', 
         icon: 'img/stats/pp/pp.webp', 
@@ -287,7 +287,7 @@ export const RESOURCE_REGISTRY = [
             };
         }
     },
-    { key: 'pp_levels', icon: 'img/stats/pp/pp.webp', singular: 'atm of Pressure', plural: 'atms of Pressure', type: 'levelStat', color: '#ff66d9', barText: 'Pressure: <span class="pp-level-value">{val}</span> atm', noTextGap: true },
+    { key: 'pp_levels', icon: 'img/stats/pp/pp.webp', singular: 'atm of Pressure', plural: 'atms of Pressure', type: 'levelStat', color: '#ff66d9', barText: 'Pressure:<span class="pp-level-value">{val}</span>atm', noTextGap: true },
 
     { key: 'cores', bgGradient: 'linear-gradient(to bottom, #0a0a0a 0%, #1a1a1a 15%, #2a2a2a 50%, #1a1a1a 85%, #0a0a0a 100%)',      icon: 'img/currencies/core/core.webp', baseIcon: 'img/currencies/core/core_plus_base.webp',   singular: 'Core',     plural: 'Cores', type: 'currency' },
     { key: 'crystals', bgGradient: 'linear-gradient(to bottom, #943276 0%, #bd53a3 15%, #e979d0 50%, #bd53a3 85%, #943276 100%)',      icon: 'img/currencies/crystal/crystal.webp', baseIcon: 'img/currencies/crystal/crystal_plus_base.webp',   singular: 'Crystal',     plural: 'Crystals', type: 'currency' },
