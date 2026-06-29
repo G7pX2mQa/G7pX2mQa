d916f8a167154971ed1640d462b34c11254867ce
DevAcc <froststolen@gmail.com>
2026-06-29 10:16:50 -0400

    Update spawner.js

--- js/game/spawner.js
+++ js/game/spawner.js
@@ -383,22 +383,22 @@ export function createSpawner(config = {}) {
                 let numToRemove = (activeItems.length - garbageCount + itemsToAdd) - maxActiveCoins;
                 for (let i = 0; i < activeItems.length && numToRemove > 0; i++) {
                     let indexToRemove = -1;
-                    if (isSurgeActive(2)) {
-                        for (let j = 0; j < activeItems.length; j++) {
-                            const c = activeItems[j];
-                            if (c && c.sizeIndex < 4) {
+                    let minSizeIndex = Infinity;
+                    
+                    for (let j = 0; j < activeItems.length; j++) {
+                        const c = activeItems[j];
+                        if (c) {
+                            const sizeIdx = c.sizeIndex || 0;
+                            if (sizeIdx < minSizeIndex) {
+                                minSizeIndex = sizeIdx;
                                 indexToRemove = j;
-                                break;
-                            }
-                        }
-                    } else {
-                        for (let j = 0; j < activeItems.length; j++) {
-                            if (activeItems[j]) {
-                                indexToRemove = j;
-                                break;
+                                if (minSizeIndex === 0) {
+                                    break;
+                                }
                             }
                         }
                     }
+
                     if (indexToRemove !== -1) {
                         removeItem(activeItems[indexToRemove], indexToRemove);
                         numToRemove--;
