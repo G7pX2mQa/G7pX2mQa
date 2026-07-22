9e8b363a9bbf45d94847b2f459824f0c296b0886
DevAcc <froststolen@gmail.com>
2026-07-22 16:55:22 -0400

    Update uiHelpers.js

--- js/util/uiHelpers.js
+++ js/util/uiHelpers.js
@@ -1,5 +1,8 @@
 export function setHtmlOrText(el, val) {
     const htmlVal = String(val);
+    if (el.__lastVal === htmlVal) return;
+    el.__lastVal = htmlVal;
+
     if (htmlVal.includes('<')) {
         el.innerHTML = htmlVal;
     } else {
