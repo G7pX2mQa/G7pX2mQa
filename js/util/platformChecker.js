bc06d2b7875a819984530334fc8abdcc647f5aae
DevAcc <froststolen@gmail.com>
2026-08-01 18:21:30 -0400

    Update platformChecker.js

--- js/util/platformChecker.js
+++ js/util/platformChecker.js
@@ -13,3 +13,13 @@ export const IS_MOBILE = (() => {
   window.IS_MOBILE = detected;
   return detected;
 })();
+
+export const IS_FIREFOX = (() => {
+  if (typeof window === 'undefined') return false;
+  if (typeof window.IS_FIREFOX !== 'undefined') {
+    return !!window.IS_FIREFOX;
+  }
+  const detected = (typeof CSS !== 'undefined' && CSS.supports && CSS.supports('-moz-appearance', 'none')) || (navigator.userAgent && navigator.userAgent.toLowerCase().includes('firefox'));
+  window.IS_FIREFOX = detected;
+  return detected;
+})();
