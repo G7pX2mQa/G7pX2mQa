c72aa30e92417179f42f2dcde757b7351d1e5147
DevAcc <froststolen@gmail.com>
2026-09-05 23:37:47 -0400

    Update cursorTrail.js

--- js/game/cursorTrail.js
+++ js/game/cursorTrail.js
@@ -541,6 +541,7 @@ export function createCursorTrail(playfield, options = {}) {
                 lastSpawnY = null;
                 lastEmitX = null;
                 lastEmitY = null;
+                pointsQueue.length = 0;
             }
         }
     };
