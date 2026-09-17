8dd286989637b4366c10c5fc8fce134f1b3fe90a
DevAcc <froststolen@gmail.com>
2026-09-17 06:43:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1958,7 +1958,6 @@ document.addEventListener("DOMContentLoaded", async () => {
             "img/misc/experiment_plus_base.webp",
             "img/misc/forge.webp",
             "img/misc/forge_plus_base.webp",
-			"img/misc/fracture_pattern.webp",
             "img/misc/green_border.webp",
             "img/misc/i.webp",
             "img/misc/infuse.webp",
