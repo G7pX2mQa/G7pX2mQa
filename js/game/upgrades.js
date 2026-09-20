670303a16cf79c6175829df1a08b15fef825ae6e
DevAcc <froststolen@gmail.com>
2026-09-20 04:42:47 -0400

    Update upgrades.js

--- js/game/upgrades.js
+++ js/game/upgrades.js
@@ -3003,7 +3003,7 @@ export const REGISTRY = [
         desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade scales similarly to Endless FP",
         
         lvlCap: 1000,
-        baseCost: "1e345",
+        baseCost: 1000,
         costType: "coins",
         upgType: "HM",
         scalingPreset: "HM",
@@ -3037,7 +3037,7 @@ export const REGISTRY = [
         desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade does not scale similarly to Endless FP",
         
         lvlCap: 1000,
-        baseCost: 1e69,
+        baseCost: 1000,
         costType: "books",
         upgType: "HM",
         scalingPreset: "HM",
@@ -3070,7 +3070,7 @@ export const REGISTRY = [
         desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade scales similarly to Endless FP",
         
         lvlCap: 1000,
-        baseCost: 1e276,
+        baseCost: 1000,
         costType: "gold",
         upgType: "HM",
         scalingPreset: "HM",
@@ -3104,7 +3104,7 @@ export const REGISTRY = [
         desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade scales similarly to Endless FP",
         
         lvlCap: 1000,
-        baseCost: 1e207,
+        baseCost: 1000,
         costType: "magic",
         upgType: "HM",
         scalingPreset: "HM",
