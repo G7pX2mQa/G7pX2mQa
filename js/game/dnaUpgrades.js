f2f3a25ee1f3bdfcff8054ab31cf05bbefaa233f
DevAcc <froststolen@gmail.com>
2026-09-20 04:04:09 -0400

    Update dnaUpgrades.js

--- js/game/dnaUpgrades.js
+++ js/game/dnaUpgrades.js
@@ -152,7 +152,7 @@ export const REGISTRY = [
     desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade scales similarly to Endless FP",
     
     lvlCap: 1000,
-    baseCost: "1e400",
+    baseCost: 1e276,
     costType: "dna",
     upgType: "HM",
     scalingPreset: 'HM',
