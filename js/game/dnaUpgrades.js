511f0c807fa6ebc01be88d56c06b607303b04a93
DevAcc <froststolen@gmail.com>
2026-09-20 01:33:48 -0400

    Update dnaUpgrades.js

--- js/game/dnaUpgrades.js
+++ js/game/dnaUpgrades.js
@@ -1,6 +1,7 @@
 import { formatMultForUi } from '../util/numFormat.js';
 import { BigNum } from "../util/bigNum.js";
 
+
 export const DNA_AREA_KEY = 'dna';
 
 export const REGISTRY = [
@@ -147,8 +148,9 @@ export const REGISTRY = [
     area: DNA_AREA_KEY,
     id: 6,
     title: "DNA Rubble Value",
+    tie: "dna_6",
     desc: "Multiplies Rubble value by 1.1x per level\nThis upgrade scales similarly to Endless FP",
-    descScale: 0.9,
+    
     lvlCap: 1000,
     baseCost: "1e500",
     costType: "dna",
