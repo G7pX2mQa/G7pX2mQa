d79448b4cfd07a5d973942f3b7667efe119feace
DevAcc <froststolen@gmail.com>
2026-09-20 21:52:44 -0400

    Update ucUpgrades.js

--- js/game/ucUpgrades.js
+++ js/game/ucUpgrades.js
@@ -661,7 +661,7 @@ export const UC_REGISTRY = [
         computeLockState() {
             let isUnlocked = false;
             try {
-                isUnlocked = lsGetItem(`ccc:collapseChallengeCompleted:stone:${getActiveSlot()}`) === "1";
+                isUnlocked = lsGetItem(`ccc:collapseChallengeCompleted:stone:${getActiveSlot() ?? "default"}`) === "1";
             } catch {}
 
             if (isUnlocked) {
