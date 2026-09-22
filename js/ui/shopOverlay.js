7a00d2535189551daf9846faf288f242ad75a1eb
DevAcc <froststolen@gmail.com>
2026-09-22 18:11:50 -0400

    Update shopOverlay.js

--- js/ui/shopOverlay.js
+++ js/ui/shopOverlay.js
@@ -2599,12 +2599,6 @@ export function openUpgradeOverlay(upgDef, mode = "standard") {
             if (costs) costs.remove();
         }
 
-        if (info.children.length === 0) {
-            info.style.display = "none";
-        } else {
-            info.style.display = "";
-        }
-
         // Milestones Row
         const milestonesContainer = upgSheetEl.querySelector(".upg-milestones");
         let milestonesRow = milestonesContainer.querySelector(".hm-view-milestones-row");
