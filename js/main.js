6e04a54aff90b406f3640c830ece4367307dbd47
DevAcc <froststolen@gmail.com>
2026-09-23 22:05:22 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2030,6 +2030,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             "img/currencies/coin/coin_plus_base.webp",
 			"img/currencies/coral/coral_red.webp",
 			"img/currencies/coral/coral_base.webp",
+			"img/currencies/coral/coral_pattern_red.webp",
 			"img/currencies/coral/coral_red_plus_base.webp",
             "img/currencies/core/core.webp",
             "img/currencies/core/core_base.webp",
